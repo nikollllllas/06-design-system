988478e0842c9cc8103dcb7bb7faf12108b5ec3c
nikollllllas <nikollasohta2014@gmail.com>
2024-05-02 08:36:35 -0300

    feat: heading

--- packages/docs/src/stories/text.stories.tsx
+++ packages/docs/src/stories/text.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Text, TextProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Thypography/Text',
+  title: 'Typography/Text',
   component: Text,
   args: {
     children:
