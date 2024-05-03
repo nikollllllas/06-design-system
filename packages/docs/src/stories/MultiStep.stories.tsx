cbee8d6d727bf99d683a675ff766e349db0366a9
nikollllllas <nikollasohta2014@gmail.com>
2024-05-03 14:25:15 -0300

    feat: multi-step

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,32 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 2,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as='label'
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+          <Text size={'sm'}>Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    size: 4,
+    currentStep: 4,
+  },
+}
