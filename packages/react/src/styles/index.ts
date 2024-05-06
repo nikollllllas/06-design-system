adab612836f996bab3c12fd0e092f8fe180181d3
nikollllllas <nikollasohta2014@gmail.com>
2024-05-06 14:46:48 -0300

    fix: up

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@ng-studies-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
---
+++ packages/ts-config/CHANGELOG.md
@@ -0,0 +1,17 @@
+# @ng-studies-ignite-ui/ts-config
+
+## 2.0.0
+
+### Major Changes
+
+- //
+
+### Patch Changes
+
+- //
+
+## 1.0.0
+
+### Major Changes
+
+- ## "@ng-studies-ignite-ui/\*: first major version"
